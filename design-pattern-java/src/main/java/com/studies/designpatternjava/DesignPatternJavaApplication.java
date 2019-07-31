0b41aabf82d3b40c267370bd180f6f99e12b6d8b
Ronaldo <guastalli31@gmail.com>
2019-07-30 22:58:39 -0300

    [FactoryMethod]
    criar projeto que estuda padrão Factory Method

---
+++ design-pattern-java/src/main/java/com/studies/designpatternjava/DesignPatternJavaApplication.java
@@ -0,0 +1,13 @@
+package com.studies.designpatternjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternJavaApplication.class, args);
+	}
+
+}

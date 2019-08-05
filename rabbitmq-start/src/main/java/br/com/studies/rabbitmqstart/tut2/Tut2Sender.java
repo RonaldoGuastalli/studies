b353cbe7576cbe90e785f5943d9250daf4516485
Ronaldo Jose Guastalli <rjguastalli@desenvvar10-lnx.dimed.local>
2019-08-05 12:31:36 -0300

    [TUTORIAL-DOIS]
    criar uma fila de trabalho com dois consumidores e cada tarefa é entregue a exatamente um determinado consumidor.

--- rabbitmq-start/src/main/java/br/com/studies/rabbitmqstart/tut2/Tut2Sender.java
+++ rabbitmq-start/src/main/java/br/com/studies/rabbitmqstart/tut2/Tut2Sender.java
@@ -21,17 +21,20 @@ public class Tut2Sender {
 
     @Scheduled(fixedDelay = 1000, initialDelay = 500)
     public void send() {
-        StringBuilder builder = new StringBuilder("Hello");
-        if(dots.incrementAndGet() == 3) {
-            dots.set(1);
-        }
+        for (int j = 0; j < 10; j++) {
+            StringBuilder builder = new StringBuilder("Hello");
+            if (dots.incrementAndGet() == 3) {
+                dots.set(1);
+            }
 
-        for (int i = 0; i < dots.get(); i++) {
-            builder.append('.');
+            for (int i = 0; i < dots.get(); i++) {
+                builder.append('.');
+            }
+            builder.append(count.incrementAndGet());
+            String message = builder.toString();
+            template.convertAndSend(queue.getName(), message);
+            System.out.println(" [x] Sent '" + message + "'");
+
         }
-        builder.append(count.incrementAndGet());
-        String message = builder.toString();
-        template.convertAndSend(queue.getName(), message);
-        System.out.println(" [x] Sent '" + message + "'");
     }
 }

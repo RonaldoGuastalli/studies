b635d41bbbaedf96a9b54ff6a53c4d6b8a8dc479
Ronaldo Jose Guastalli <rjguastalli@desenvvar10-lnx.dimed.local>
2019-08-09 13:00:31 -0300

    [TUTORIAL-NOVE]
    uso de remote procedure call e replyTo para criar uma fila temporária.

---
+++ rabbitmq-start/src/main/java/br/com/studies/rabbitmqstart/tut9/ClienteDois.java
@@ -0,0 +1,76 @@
+package br.com.studies.rabbitmqstart.tut9;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
+public class ClienteDois implements AutoCloseable {
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+
+    //inicialização
+    public ClienteDois() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+
+    public static void main(String[] args) {
+        // Objeto no cliente
+        String request = "mensagem do CLIENTE 2";
+        try {
+            ClienteDois cliente = new ClienteDois();
+            String response = cliente.call(request);
+            System.out.println(" [.] Got '" + response + "'");
+
+        } catch (IOException | TimeoutException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String call(String message) throws IOException, InterruptedException {
+        // Gerar id
+        final String coorId = UUID.randomUUID().toString();
+        // Nome da queue de reply
+        String replyQueueName = channel.queueDeclare().getQueue();
+        // Setando as props no servidor
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(coorId)
+                .replyTo(replyQueueName)
+                .build();
+        // Publicar na fila reply
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        //
+        final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
+        // Consumidor da reply, do servidor
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(coorId)) {
+                response.offer(new String(delivery.getBody(), "UTF-8"));
+            }
+        }, consumerTag -> {
+        });
+
+        String result = response.take();
+        channel.basicCancel(ctag);
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        connection.close();
+    }
+}
--- rabbitmq-start/src/main/java/br/com/studies/rabbitmqstart/tut9/Server.java
+++ rabbitmq-start/src/main/java/br/com/studies/rabbitmqstart/tut9/Server.java
@@ -2,48 +2,60 @@ package br.com.studies.rabbitmqstart.tut9;
 
 import com.rabbitmq.client.*;
 
-import java.nio.charset.StandardCharsets;
-
 public class Server {
 
     private static final String RPC_QUEUE = "rpc_queue";
 
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
 
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+        try(Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()){
 
-            // Cria a fila fixa, caso não exista
+            // Declara a fila, caso ela não exista
             channel.queueDeclare(RPC_QUEUE, false, false, false, null);
+
             System.out.println(" [x] Awaiting RPC requests");
 
-            // Response, objeto da mensagem
-            Produto produto = Produto.builder()
-                    .id(1)
-                    .nome("Paracetamos")
-                    .valor("R$ 100,00")
-                    .build();
+            // Objeto trabalhado no servido
+            String response = "mensagem modificada no servido";
 
-            // Callback das mensagens da fila, setando o correlationId que veio pela msg
+            Object monitor = new Object();
+
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                // Parametros do server para o Id
                 AMQP.BasicProperties replyProps = new AMQP.BasicProperties
                         .Builder()
                         .correlationId(delivery.getProperties().getCorrelationId())
                         .build();
-                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-                System.out.println("[x] PDV Recebeu mensagem do servidor '" + message + "'");
+                // Publicar na fila retly
 
-                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, produto.toString().getBytes("UTF-8"));
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+
+                //
+                synchronized (monitor) {
+                    monitor.notify();
+                }
             };
 
-            //consumindo as mensagens da fila
+            //
             channel.basicConsume(RPC_QUEUE, false, deliverCallback, consumerTag -> {});
+            //
+            while(true) {
+                synchronized (monitor) {
+                    try {
+                        monitor.wait();
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
 
         }
+
     }
 }
